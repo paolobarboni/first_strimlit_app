7a9327b5c9c47e808b438ceffc2605d6a7a61002
paolobarboni <152096047+paolobarboni@users.noreply.github.com>
2023-11-30 12:51:30 +0100

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -29,9 +29,16 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
-import snowflake.connector
-
 # convert json in tabular format 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shiw the converted json data as a table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
